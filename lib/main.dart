301785fe9c2758d2f66610a7ebc7401df97de647
Eman-Bakri <eman1996-sy@hotmail.com>
2025-01-26 22:08:41 +0300

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:bookme_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'app.dart';
 
 ///------ Entry point of Book me app -----
-void main() {
+Future<void> main() async {
 
-
+// Todo: Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform).then(
+          (FirebaseApp value) => Get.put/*AuthenticationRepository()*/);
   runApp(const MyApp());
 }
